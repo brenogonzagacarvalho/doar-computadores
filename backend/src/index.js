5bcd9de672f71bff303c6c129785918fe63ba2eb
Breno Gonzaga de Carvalho <31925096+brenogonzagacarvalho@users.noreply.github.com>
2022-07-27 18:35:42 -0300

    rota post

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-const app = require('../server')
+const app = require('./server')
 
 
 app.listen(3000, () => console.log('Rodando na https://localhost:3000'));
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res) => {
-    res.json({alive: true})
-    res.sendStatus(200)
+app.get('/', (req, res) => {
+  res.json({
+    alive: true
+  })
+  res.sendStatus(200)
+})
+
+
+
+//'https://www.doar-computador-api.herokuapp.com/donation'
+app.post('/data', (req, res) => {
+  
+  const data = {
+    "error": true,
+    "requiredFields": [
+      "name",
+      "phone",
+      "zip",
+      "city",
+      "state",
+      "streetAddress",
+      "number",
+      "complement",
+      "neighborhood",
+      "deviceCount",
+      "devices"
+    ],
+    errorMessage: "Todos os campos obrigatórios devem ser informados"
+  }  
+
+  return res.json(data);
 })
 
 
